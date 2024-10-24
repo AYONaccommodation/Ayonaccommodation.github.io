cc66dd8a8f65ae912f44485c31ee8a7292e62db7
AYONaccommodation <ayonaccommodation@gmail.com>
2024-10-24 08:24:09 +0100

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 // console.log("nav");
-const nav_links=document.querySelector(".nav-links");
-const menuBtn_open=document.querySelector("#menu-btn_open");
-const menuBtn_close=document.querySelector("#menu-btn_close");
+const nav_links = document.querySelector(".nav-links");
+const menuBtn_open = document.querySelector("#menu-btn_open");
+const menuBtn_close = document.querySelector("#menu-btn_close");
 // console.log(nav_links, menuBtn_open, menuBtn_close);
 menuBtn_open.onclick = () => toggler();
 menuBtn_close.onclick = () => toggler();
